70d156359b40771a8eaaea99718780f4701de6e6
Phebedevadattam <64638240+Phebedevadattam@users.noreply.github.com>
2021-10-06 14:40:07 +0530

    Create convert complex numbers to Polar coordinates.py

---
+++ convert complex numbers to Polar coordinates.py
@@ -0,0 +1,10 @@
+# Python code to implement
+# the polar()function
+		
+# importing "cmath"
+# for mathematical operations
+import cmath
+	
+# using cmath.polar() method
+num = cmath.polar(1)
+print(num)
